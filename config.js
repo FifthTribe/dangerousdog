9edd49d445aca44c402adbc0aa04f3cca9700fba
mandazi <amir@new-host-2.home>
2015-10-12 14:35:46 -0400

    Crap. Should not have committed this.

--- config.js
+++
@@ -1,4 +0,0 @@
-var config = {};
-process.env.PARSEAPPID = 'y8nvFK7EAjKBWirFUvJ8XNqBSOpLA26NxlmNds3x';
-process.env.PARSEJSKEY = 'YQt7R7TzuMpjZvRMw472z8pwMjrwM3y6eI12FAC6';
-module.exports = config;
