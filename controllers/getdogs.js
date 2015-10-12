3e40d288e32e7493b43c19c962e71c0df2600ab6
mandazi <amir@new-host.home>
2015-10-12 16:32:16 -0400

    parsing all the urls from the dogs.txt file

--- controllers/getdogs.js
+++ controllers/getdogs.js
@@ -1,122 +1,136 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
 var Parse = require('parse/node');
 Parse.initialize(process.env.PARSEAPPID, process.env.PARSEJSKEY);
 
 module.exports.pullData = function(req, res){
 
-  // The URL to scrape
-  var url = 'https://dd.va-vdacs.com/public/public.cgi?sysdogno=1161&submit=detail';
+  // Get the list of all the URLs needed to parse
+  fs.readFile("dogs.txt", 'utf8', function(err, data) {
+    if (err) throw err;
+    var urls = data.split('\n');
 
-  // Get the URL
-  request(url, function(error, response, html){
+    for (var i = 0; i < urls.length; i++){
+      var url = urls[i].replace(/\s/, "");
 
-    if(!error) {
+      // Get the URL
+      request(url, function(error, response, html){
 
-      // Get the HTML from cheerio
-      var $ = cheerio.load(html);
+        if(!error) {
+          console.log('url',url);
 
-      var Dog = Parse.Object.extend("Dog");
-      var dog = new Dog();
+          // Get the HTML from cheerio
+          var $ = cheerio.load(html);
 
-      // Get the owner info (address)
-      $('#ajaxcontentOwner').filter(function(){
+          var Dog = Parse.Object.extend("Dog");
+          var dog = new Dog();
 
-        var ownerData = $(this);
-        var ownerAddress = $('p',ownerData).get(2);
+          // Get the owner info (address)
+          $('#ajaxcontentOwner').filter(function(){
 
-        // Loop though each child object of the p tag object to get the text
-        for (var x = 0; x < ownerAddress.children.length; x++){
-          var ownerInfoItem = ownerAddress.children[x];
-          if (ownerInfoItem.type === "text"){
-            var ownerDetails = ownerInfoItem.data.replace(/\s+/g, " ").replace(/^\s|\s$/g, "");
+            var ownerData = $(this);
+            var ownerAddress = $('p',ownerData).get(2);
 
-            // Owner name
-            if ( x === 2){
-              dog.set("owner",ownerDetails);
+            // Loop though each child object of the p tag object to get the text
+            for (var x = 0; x < ownerAddress.children.length; x++){
+              var ownerInfoItem = ownerAddress.children[x];
+              if (ownerInfoItem.type === "text"){
+                var ownerDetails = ownerInfoItem.data.replace(/\s+/g, " ").replace(/^\s|\s$/g, "");
+
+                // Owner name
+                if ( x === 2){
+                  dog.set("owner",ownerDetails);
+                }
+
+                // Street address
+                if ( x === 4){
+                  dog.set("streetAddress",ownerDetails);
+                }
+
+                // City, state zip
+                if ( x === 6 ){
+                  dog.set("cityStateZip",ownerDetails);
+                }
+
+              }
             }
 
-            // Street address
-            if ( x === 4){
-              dog.set("streetAddress",ownerDetails);
+
+
+          });
+
+          $('#ajaxcontentDog').filter(function(){
+
+            // Get the entire DOM
+            var data = $(this);
+
+            // Get the county and the number of dog
+            var countyDogNumberInfo = $('p span.body3',data).get(0);
+
+            //Loop through each child object of the p tag with the county and number of dog and get the text
+            for (var x = 0; x < countyDogNumberInfo.children.length; x++){
+              var countyDogNumberInfoItem = countyDogNumberInfo.children[x];
+              if (countyDogNumberInfoItem.type === "text"){
+                // The text will be split by a :
+                var countNumberText = countyDogNumberInfoItem.data.replace(/\s+/g, " ").replace(/^\s|\s$/g, "").split(':');
+                dog.set("county",countNumberText[0]);
+                dog.set("numberText",countNumberText[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
+                //dog.set("number",parseInt(countNumberText[1].match(/\d/g).join('')));
+              }
             }
 
-            // City, state zip
-            if ( x === 6 ){
-              dog.set("cityStateZip",ownerDetails);
+            // Get the p tag with the dog information
+            var dogInfoParagraph = $('p',data).get(2);
+
+            // Loop though each child object of the p tag object to get the text
+            for (var x = 0; x < dogInfoParagraph.children.length; x++){
+              var dogInfoItem = dogInfoParagraph.children[x];
+              if (dogInfoItem.type === "text"){
+                var dogDetails = dogInfoItem.data.replace(/\s+/g, " ").replace(/^\s|\s$/g, "").split(':');
+                if ( dogDetails[0] === 'Name of Dangerous Dog'){
+                  dog.set("name",dogDetails[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
+                }
+                if ( dogDetails[0] === 'Primary Breed'){
+                  dog.set("primaryBreed",dogDetails[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
+                }
+                if ( dogDetails[0] === 'Secondary Breed'){
+                  dog.set("secondaryBreed",dogDetails[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
+                }
+                if ( dogDetails[0] === 'Color and Markings'){
+                  dog.set("colorAndMarkings",dogDetails[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
+                }
+              }
             }
 
-          }
+          });
+
+
+          dog.save(null, {
+            success: function(dog) {
+              // Execute any logic that should take place after the object is saved.
+              console.log('New object created with objectId: ' + dog.id);
+            },
+            error: function(gameScore, error) {
+              // Execute any logic that should take place if the save fails.
+              // error is a Parse.Error with an error code and message.
+              console.log('Failed to create new object, with error code: ' + error.message);
+            }
+          });
+
+
+        } else {
+          console.log('error',error);
         }
-
-
-
       });
 
-      $('#ajaxcontentDog').filter(function(){
 
-        // Get the entire DOM
-        var data = $(this);
-
-        // Get the county and the number of dog
-        var countyDogNumberInfo = $('p span.body3',data).get(0);
-
-        //Loop through each child object of the p tag with the county and number of dog and get the text
-        for (var x = 0; x < countyDogNumberInfo.children.length; x++){
-          var countyDogNumberInfoItem = countyDogNumberInfo.children[x];
-          if (countyDogNumberInfoItem.type === "text"){
-            // The text will be split by a :
-            var countNumberText = countyDogNumberInfoItem.data.replace(/\s+/g, " ").replace(/^\s|\s$/g, "").split(':');
-            dog.set("county",countNumberText[0]);
-            dog.set("numberText",countNumberText[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
-            dog.set("number",parseInt(countNumberText[1].match(/\d/g).join('')));
-          }
-        }
-
-        // Get the p tag with the dog information
-        var dogInfoParagraph = $('p',data).get(2);
-
-        // Loop though each child object of the p tag object to get the text
-        for (var x = 0; x < dogInfoParagraph.children.length; x++){
-          var dogInfoItem = dogInfoParagraph.children[x];
-          if (dogInfoItem.type === "text"){
-            var dogDetails = dogInfoItem.data.replace(/\s+/g, " ").replace(/^\s|\s$/g, "").split(':');
-            if ( dogDetails[0] === 'Name of Dangerous Dog'){
-              dog.set("name",dogDetails[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
-            }
-            if ( dogDetails[0] === 'Primary Breed'){
-              dog.set("primaryBreed",dogDetails[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
-            }
-            if ( dogDetails[0] === 'Secondary Breed'){
-              dog.set("secondaryBreed",dogDetails[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
-            }
-            if ( dogDetails[0] === 'Color and Markings'){
-              dog.set("colorAndMarkings",dogDetails[1].replace(/\s+/g, " ").replace(/^\s|\s$/g, ""));
-            }
-          }
-        }
-
-      });
-
-
-      dog.save(null, {
-        success: function(dog) {
-          // Execute any logic that should take place after the object is saved.
-          console.log('New object created with objectId: ' + dog.id);
-        },
-        error: function(gameScore, error) {
-          // Execute any logic that should take place if the save fails.
-          // error is a Parse.Error with an error code and message.
-          console.log('Failed to create new object, with error code: ' + error.message);
-        }
-      });
-
-      res.send('got dog info:<br />'+JSON.stringify(dog));
-
-    } else {
-      console.log('error',error);
-      res.send('Error:<br />'+error);
     }
+
+    res.send('all dogs have been added');
+
+
   });
+
 
 }
